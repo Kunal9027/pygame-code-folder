fd1a235f1ebe8314395a16b593141bdb54102075
kunal9027 <kunal902770@gmail.com>
2022-12-25 19:48:01 +0530

    new changes

--- first pygame file.py
+++ first pygame file.py
@@ -7,30 +7,36 @@ screen = pygame.display.set_mode(
 )  # screen = command is use for change screen size
 
 
-color = 150, 190, 250  # Here R=150 , G=190 , B=250 RGB full form is RED GREEN BLUE
+COLOR = 150, 190, 250  # Here R=150 , G=190 , B=250 RGB full form is RED GREEN BLUE
 
 
 pygame.display.set_caption(
     " Game made by Kunal Chaudhary "
 )  # set_caption is used to change  title of the screen
 
-# its main function for running window
+colck = pygame.time.Clock()
+FPS = 30
+
+
+def draw_win():
+    screen.fill(COLOR)  # screen.fill is used to change bg_color
+    pygame.display.flip()  # pygame.display.flip is used for update
+
+
+def kunal():
+    running = True
+    while running:
+        colck.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        draw_win()
+
+    pygame.quit()
+    exit()
+
 
 if __name__ == "__main__":
 
-    def kunal():
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
-            pygame.display.flip()  # pygame.display.flip is used for update
-
-            screen.fill(color)  # screen.fill is used to change bg_color
-
-        pygame.quit()
-        exit()
-
-
-kunal()
+    kunal()
