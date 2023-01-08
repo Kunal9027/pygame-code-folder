51646369890fe2ee876ce300bc640a8758fc6c91
kunal9027 <kunal902770@gmail.com>
2023-01-08 19:54:39 +0530

    only sounds left

--- first pygame file.py
+++ first pygame file.py
@@ -1,5 +1,7 @@
 import pygame
 import os
+pygame.font.init()
+pygame.mixer.init()
 
 pygame.init()  # it initialize all pygame module
 
@@ -8,6 +10,7 @@ screen = pygame.display.set_mode((WIDTH ,HEIGHT))  # screen = command is use for
 pygame.display.set_caption(" Game made by Kunal Chaudhary ")  # set_caption is used to change title of the screen
 
 COLOR = 150, 190, 250  # Here R=150 , G=190 , B=250 RGB full form is RED GREEN BLUE
+WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 YELLOW = (255,255,0)
@@ -21,6 +24,11 @@ FPS = 30
 SPEED_SHIP = 10
 SPEED_BULLET = 15
 MAX_bullets = 4
+
+
+HEALTH_FONT = pygame.font.SysFont('comicsans', 30)
+WINNER_FONT  = pygame.font.SysFont('comcsans' , 75)
+
 
 YELLOW_HIT = pygame.USEREVENT + 1
 RED_HIT  =pygame.USEREVENT + 2
@@ -36,10 +44,17 @@ RED_player_image = pygame.image.load(os.path.join("assets/spaceship_red.png")) #
 RED_size = pygame.transform.scale(RED_player_image, (SHIP_WIDTH,SHIP_HIGHT))  #scale is used to resiz image 
 RED_rotate = pygame.transform.rotate(RED_size , 270)  # transform.rotate is used to rotate image 
 
-def draw_win(red , yellow , red_bullet , yellow_bulllet):
+def draw_win(red , yellow , red_bullet , yellow_bulllet , red_health ,yellow_health):
     #screen.fill(COLOR)  # screen.fill is used to change bg_color
     screen.blit(BACKGROUND_IMAGE,(0,0))
     pygame.draw.rect(screen , BLACK , BORDER)
+
+    red_health_text = HEALTH_FONT.render('health:' + str(red_health), 1 , COLOR)
+    yellow_health_text = HEALTH_FONT.render('health:' + str(yellow_health), 1 , COLOR)
+
+    screen.blit(red_health_text , (400 , 10))
+    screen.blit(yellow_health_text , (10, 10))
+
     screen.blit(YELLOW_rotate,(yellow.x , yellow.y ))    #blit is use to put image on screen  
     screen.blit(RED_rotate ,(red.x ,red.y))
 
@@ -72,6 +87,15 @@ def MANAGE_BULLETS(YELLOW_bullet,RED_bullet,yellow,red):
         elif bullet.x < 0:
             RED_bullet.remove(bullet)
 
+
+def draw_winner(text):
+    draw_text = WINNER_FONT.render(text , 1 , COLOR)
+    screen.blit(draw_text, (150 , 200))
+    pygame.display.update()
+    pygame.time.delay(5000)
+
+
+
 def kunal():
 
     red = pygame.Rect(400,150 ,SHIP_WIDTH ,SHIP_HIGHT) # RECT is use to make a rectangle 
@@ -80,18 +104,19 @@ def kunal():
     RED_bullet = []
     YELLOW_bullet = []
 
-    yellow_health = 10
     red_health = 10
+    yellow_health = 10
 
     running = True
     while running:
-        colck.tick(FPS)
+        colck.tick(FPS)                 # clock is use to set fps how many photos update per sec.
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:  #event is to use make condition on certain event
                 running = False
+                pygame.quit()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LCTRL and len(YELLOW_bullet) < MAX_bullets :
+            if event.type == pygame.KEYDOWN:  # key down event repest key is pressed
+                if event.key == pygame.K_LCTRL and len(YELLOW_bullet) < MAX_bullets :   #
                     bullet = pygame.Rect(yellow.x + SHIP_WIDTH,yellow.y + SHIP_HIGHT/2 -3 , 10 ,5)
                     YELLOW_bullet.append(bullet)
 
@@ -100,11 +125,11 @@ def kunal():
                    bullet = pygame.Rect(red.x ,red.y + SHIP_HIGHT/2 -3 , 10 ,5)
                    RED_bullet.append(bullet)
 
-        if event.type == RED_HIT:
-            red_health -= 1
+            if event.type == RED_HIT:
+                red_health -= 1
 
-        if event.type == YELLOW_HIT:
-            yellow_health -= 1
+            if event.type == YELLOW_HIT:
+                yellow_health -= 1
 
         winner_text = ""
         if red_health <= 0:
@@ -114,7 +139,8 @@ def kunal():
             winner_text = "RED won"
 
         if winner_text != "":
-            pass #SOMEONE WON 
+            draw_winner(winner_text) 
+            break
 
         press_button = pygame.key.get_pressed()
         #YELLOW
@@ -122,9 +148,9 @@ def kunal():
             yellow.x -=SPEED_SHIP
         if press_button[pygame.K_d] and yellow.x + 50 < 295 : #RIGHT
             yellow.x +=SPEED_SHIP
-        if press_button[pygame.K_w] and yellow.y -4 >0: #UP
+        if press_button[pygame.K_w] and yellow.y -4 > 0: #UP
             yellow.y -=SPEED_SHIP
-        if press_button[pygame.K_s] and yellow.y +5<455: #DOWN
+        if press_button[pygame.K_s] and yellow.y +5 < 455: #DOWN
             yellow.y +=SPEED_SHIP
         #RED
         if press_button[pygame.K_LEFT] and red.x - 4 > 305 :  
@@ -138,10 +164,11 @@ def kunal():
 
         MANAGE_BULLETS(YELLOW_bullet,RED_bullet,yellow,red)
     
-        draw_win(red , yellow, RED_bullet , YELLOW_bullet)
+        draw_win(red , yellow, RED_bullet , YELLOW_bullet, red_health ,yellow_health)
+        
 
-    pygame.quit()
-    exit()
+    kunal()
+    
 
 
 if __name__ == "__main__":
